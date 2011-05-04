8834bfe62257eed9d194b4f5b79d3b925b314765
Attila Szegedi <szegedia@gmail.com>
2011-05-04 16:26:49 -0700

    Make sure we delegate all log formatting to actual log framework

--- core/ri/src/main/java/org/jredis/ri/alphazero/support/Log.java
+++ core/ri/src/main/java/org/jredis/ri/alphazero/support/Log.java
@@ -32,17 +32,17 @@ public class Log {
 
 	// the various 'just FYI's ...
 	public static final void log (String msg)   { log (msg, (Object[])null); }
-	public static final void log (String format, Object...args)   { 
-		logger.info(String.format(format, args)); 
+	public static final void log (String format, Object...args)   {
+	  logger.log(Level.INFO, format, args);
 	}
 	public static final void debug (String msg) { debug(msg, (Object[])null); }
 	public static final void debug (String format, Object...args) { 
-		logger.log(Level.FINE, String.format(format, args)); 
+		logger.log(Level.FINE, format, args);
 	}
 	
 	// the various 'error! run for covers', ... 
 	public static final void error (String msg)   { _error (Category.ERROR, msg); }
-  public static final void error (String msg, Throwable t)   { logger.log(Level.SEVERE, msg, t); }
+	public static final void error (String msg, Throwable t)   { logger.log(Level.SEVERE, msg, t); }
 	public static final void error (String msg, Object...args) { _error (Category.ERROR, msg, args); }
 	
 	public static final void problem (String msg) { _error (Category.PROBLEM, msg); }
@@ -52,10 +52,13 @@ public class Log {
 	public static final void bug (String msg, Object...args) { _error (Category.BUG, msg, args); }
 	
 	private static final void _error (Category cat, String msg, Object...args) {
-		msg = String.format(msg, args);
 		if(cat.equals(Category.ERROR))
-			logger.severe(String.format("%s", msg));
-		else
-			logger.log(Level.WARNING, String.format("%s: %s", cat, msg));
+			logger.log(Level.SEVERE, msg, args);
+		else {
+		  Object[] newargs = new Object[args.length + 1];
+		  System.arraycopy(args, 0, newargs, 1, args.length);
+		  newargs[0] = cat;
+		  logger.log(Level.WARNING, String.format("%s: " + msg, newargs));
+		}
 	}
 }
